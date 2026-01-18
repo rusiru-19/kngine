5b6b83ba89c4221fc3a46a186497e7455f03c5f6
Rusiru Thamara Kaluarachchi <122989375+rusiru-19@users.noreply.github.com>
2026-01-18 17:16:11 +0530

    favicons added

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,6 +102,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${hackedfont.variable} antialiased `}
       >
